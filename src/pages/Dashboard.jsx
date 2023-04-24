2781434246ce1cc75670c655d8e0ab6073afda31
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-24 22:58:54 +0000

    Components and style props

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,31 @@
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+
 export default function Dashboard() {
+  const boxStyles = {
+    p: "10px",
+    bg: "purple",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    filter: "blur(2px)",
+    ":hover": {
+      color: "black",
+      bg: "blue.200",
+    },
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra UI component
+      </Heading>
+      <Text marginLeft="30px">Hi there</Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Hi there
+      </Text>
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is Romane!</Text>
+      </Box>
+      <Box sx={boxStyles}>Hello, Ninjas!</Box>
+    </Container>
+  );
 }
