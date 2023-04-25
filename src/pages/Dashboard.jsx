2ace324602a148bbd0d4525d5b4b867521f38637
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-25 00:03:36 +0000

    Using the grid component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,22 @@
-import { Container, Heading, Text, Box } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
-  const boxStyles = {
-    p: "10px",
-    bg: "purple",
-    color: "white",
-    m: "10px",
-    textAlign: "center",
-    filter: "blur(2px)",
-    ":hover": {
-      color: "black",
-      bg: "blue.200",
-    },
-  };
   return (
-    <Container as="section" maxWidth="4xl" py="20px">
-      <Heading my="30px" p="10px">
-        Chakra UI component
-      </Heading>
-      <Text marginLeft="30px">Hi there</Text>
-      <Text ml="30px" color="blue.300" fontWeight="bold">
-        Hi there
-      </Text>
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is Romane!</Text>
-      </Box>
-      <Box sx={boxStyles}>Hello, Ninjas!</Box>
-    </Container>
+    <SimpleGrid p="10px" spacing={10} minChildWidth="250px">
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+    </SimpleGrid>
   );
 }
